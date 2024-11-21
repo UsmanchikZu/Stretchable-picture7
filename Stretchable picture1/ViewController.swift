9d4488a4d05184b4c95bede68524855ba8051fce
Zuleikha Usmanova <>
2024-11-21 20:51:05 +0300

    add

---
+++ Stretchable picture1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stretchable picture1
+//
+//  Created by aeroclub on 21.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
